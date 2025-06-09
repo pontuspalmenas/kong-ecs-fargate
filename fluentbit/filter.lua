1187e625d36fa1aa7092d4f21631e8eb84994552
Pontus Palmenäs <pontus.palmenas@konghq.com>
2025-06-09 11:19:24 +0200

    Updated comment to clarify intent

--- fluentbit/filter.lua
+++ fluentbit/filter.lua
@@ -8,11 +8,9 @@ local HEADERS_TO_REDACT = {
 }
 
 -- Redact headers defined in HEADERS_TO_REDACT.
--- Define headers in lowercase, and the function below will lowercase check them.
+-- Define headers in lowercase, and this function will lowercase-check them.
 -- Original casing will be kept in log for readability.
 function redact_headers(tag, timestamp, record)
-    --record._lua_debug = "record: " .. record.request
-
     if record.request.headers then
        for _, key in ipairs(HEADERS_TO_REDACT) do
             if record.request.headers[string.lower(key)] ~= nil then
